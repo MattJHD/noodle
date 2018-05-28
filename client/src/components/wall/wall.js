d1892db58c646bdc82f2b39625058364df0521fd
Svanaks <bloch.william@gmail.com>
2018-05-29 00:58:39 +0200

    modal + dl button
    corrections modal + dl button + user infos on wall page

--- client/src/components/wall/wall.js
+++ client/src/components/wall/wall.js
@@ -3,11 +3,32 @@ import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getCurrentWall } from "../../actions/wallActions";
 // import { logoutUser } from "../../actions/authActions";
+import Spinner from "../common/Spinner";
 import "./wall.css";
 
 import WallItem from "./wallItem";
 
 class Wall extends Component {
+  displayModal(i, e) {
+    var modal = document.getElementById(i);
+
+    // Get the image and insert it inside the modal - use its "alt" text as a caption
+    var img = document.getElementById(e.target.src);
+    var modalImg = document.getElementById(e.target.src + i);
+    var captionText = document.getElementById("caption" + i);
+    console.log(captionText);
+
+    modal.style.display = "block";
+    modalImg.src = e.target.src;
+    captionText.innerHTML = e.target.alt;
+  }
+
+  closeModal(i, e) {
+    var modal = document.getElementById(i);
+    var span = document.getElementsByClassName("close")[0];
+    modal.style.display = "none";
+  }
+
   componentDidMount() {
     // if (!this.props.auth.isAuthenticated) {
     //   this.props.history.push("/");
@@ -18,27 +39,60 @@ class Wall extends Component {
   render() {
     const { user } = this.props.auth;
     const { wall, loading } = this.props.wall;
-    console.log(user);
-    console.log(wall);
+    // console.log(user);
+    // console.log(wall);
     let wallContent;
 
     if (wall === null || loading) {
-      wallContent = <h4 className="center">Loading...</h4>;
+      wallContent = <Spinner />;
     } else {
       if (Object.keys(wall).length > 0) {
         wallContent = (
           <div>
-            <div className="sideBar" />
+            <div className="sideBar">
+              <div className="userInfo">
+                <div className="Wall">{user.username}</div>
+                <div className="Wall">
+                  {user.firstName} {user.lastName}
+                </div>
+                <div className="Wall">{user.email}</div>
+              </div>
+            </div>
             <div className="wall-container">
-              <div className="Wall">{user.id}</div>
-              <div className="Wall">{user.email}</div>
-              <div className="Wall">{user.firstName}</div>
-              <div className="Wall">{user.lastName}</div>
               {wall.img.map((image, i) => {
                 return (
-                  <a href={image} download="filename.png">
-                    <img src={image} alt="test" className="img-wall" />
-                  </a>
+                  <div className="img-wall">
+                    <img
+                      id={image}
+                      src={image}
+                      alt="test"
+                      // onClick={this.displayModal.bind(this, i)}
+                      onClick={this.displayModal.bind(this, i)}
+                      className="img-hover"
+                    />
+                    {/* <span className="download-img"> */}
+                    <a
+                      className="download-img"
+                      href={image}
+                      download="filename.png"
+                    >
+                      ↓
+                    </a>
+                    {/* </span> */}
+                    <div id={i} className="modal">
+                      <span
+                        className="close"
+                        onClick={this.closeModal.bind(this, i)}
+                      >
+                        &times;
+                      </span>
+                      <img className="modal-content" id={image + i} />
+                      <div id={"caption" + i} />
+                    </div>
+                  </div>
+                  //{/* <a href={image} download="filename.png">
+                  //<img src={image} alt="test" className="img-wall" />
+                  //</a> */
                 );
               })}
             </div>
